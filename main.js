89c445b1db50d38091f4e9b67e206bd903f0890b
JasonMacbook <jason.boden2072@outlook.com>
2025-09-13 21:25:08 +1000

    making strings & console.logs zzz

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log('Hi There!');
+console.log('I am excited to talk to you.');
+
+let bot;
+let botLocation;
+
+bot = 'teacherBot';
+botLocation = 'The Universe';
+
+console.log('Allow me to introduce myself.');
+
+let botIntroduction;
+botIntroduction = 'My name is ' + bot + ".";
+
+console.log(botIntroduction);
+
+let botLocationSentence;
+botLocationSentence = "I live in " + botLocation + ".";
+
+bot = "professorBot";
